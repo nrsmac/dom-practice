324a825ab372d046541dbb8a68e7353385fe1fd2
nrsmac <noahrschill@gmail.com>
2018-02-19 21:50:31 -0700

    initial commit to nodeGroup branch

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,7 @@
-const container = document.getElementById('container')
+const buttons = document.querySelectorAll('button');
 
-const p = document.createElement('p');
-
-container.appendChild(p);
-
-p.textContent = "Hey I'm Red!";
-p.style.color='red';
-
-const h3 = document.createElement('h3');
-container.appendChild(h3);
-h3.textContent="I'm a blue h3!";
-h3.style.color='blue';
-
-const div = document.createElement('div');
-
-
-const h1 = document.createElement('h1');
-h1.textContent = "I'm in a div";
-div.appendChild(h1);
-
-const divP = document.createElement('p');
-divP.textContent = "me too";
-div.appendChild(divP);
-
-
-container.appendChild(div);
-div.style.cssText="border: 1px dashed black; background-color: pink";
-function alertFunction () {
-  alert('hello world!');
-}
-
-
-var btn = document.querySelector('#btn');
-
-btn.addEventListener('click', function(e){
-  e.target.style.background = 'blue'
-})
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    alert(button.id);
+  });
+});
